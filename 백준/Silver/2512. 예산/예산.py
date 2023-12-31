9834dbf7111c6d4a2dd2a2931cadbbeec0f8ddbe
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2023-12-31 22:51:05 +0900

    [Silver II] Title: 예산, Time: 60 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,22 @@
+
+n = int(input())
+money = list(map(int, input().split()))
+m = int(input())
+
+start, end = 0, max(money)
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+
+    for i in money:
+        if i >= mid:
+            total += mid
+        else:
+            total += i
+
+    if total <= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
