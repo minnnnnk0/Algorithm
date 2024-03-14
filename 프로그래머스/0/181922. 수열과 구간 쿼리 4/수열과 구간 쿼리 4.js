da4d0ae2a072fa090deab95ad160fbd7e32f00ed
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-03-14 19:55:09 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 6.20 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,12 @@
+function solution(arr, queries) {
+    for (i=0; i < queries.length; i++) {
+        let [s, e, k] = queries[i];
+        
+        for (j=s; j <= e; j++) {
+            if(j % k === 0) {
+                arr[j]++;
+            }
+        }
+    }
+    return arr;
+}
