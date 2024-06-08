f4e0d11b008a9c15edb7e2c0d035f2c1de5de643
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-06-08 13:52:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.73 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,11 @@
+function solution(A,B){
+    var ans = 0;
+    
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    
+    for(let i = 0; i < A.length; i++) {
+        ans += A[i] * B[i]
+    }
+    return ans;
+}
