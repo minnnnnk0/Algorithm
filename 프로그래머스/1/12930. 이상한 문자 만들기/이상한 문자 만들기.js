390efa67c9f0cb82b6f38ad21ccb3f873e32aca7
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-08-13 23:48:57 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    let answer = '';
+    let arr = s.split(' ');
+    let str = '' ;
+    
+    for ( let i = 0; i < arr.length; i++ ) {
+        str = arr[i]
+        
+        for ( let j =0; j < str.length; j++ ) {
+            
+            if ( j % 2 ===0 ) {
+                answer += str[j].toUpperCase();
+            } else {
+                answer += str[j].toLowerCase();       
+            }
+        }
+        answer += " ";
+    }
+    answer = answer.substr( 0, answer.length - 1 );
+    return answer;
+}
