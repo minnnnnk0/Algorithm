9f8d867b37f199dd7c311724c64fa1a7895ec5fc
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-10-04 20:01:04 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,11 @@
+function solution(food) {
+    let arr = [];
+    
+    food.map((e, i) => {
+        if (e > 1) {
+        arr.push(i.toString().repeat(Math.floor(e / 2)));
+        }
+    });
+    // console.log(arr)
+    return arr.join("") + "0" + arr.reverse().join("");
+}
