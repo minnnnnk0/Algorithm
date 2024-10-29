9fead7433be175c61c1d594a11d45871c6e05baf
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-10-29 19:26:53 +0900

    [level 2] Title: 숫자 변환하기, Time: 28.77 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,20 @@
+function solution(x, y, n) {
+    
+   let dp = Array(y + 1).fill(Infinity);
+    dp[x] = 0
+    
+    for(let i = x; i <= y; i++) {
+    
+        if((i - n) >= x) {
+            dp[i] = Math.min(dp[i], dp[i - n] + 1)
+        }
+        if (i % 2 === 0) {
+            dp[i] = Math.min(dp[i], dp[i / 2] + 1)
+        }
+        if (i % 3 === 0) {
+            dp[i] = Math.min(dp[i], dp[i / 3] + 1)
+        }
+    }
+    
+    return dp[y] === Infinity ? -1 : dp[y]
+}
