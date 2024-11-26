12b6a1438705972ddc2f5cde24f1140e864998d4
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-11-27 00:22:56 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 22.48 ms, Memory: 49.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -0,0 +1,39 @@
+function solution(enroll, referral, seller, amount) {
+    const referralMap = {}
+    const recommendMap = {}
+    
+    const N = enroll.length
+    const money = Array(N).fill(0)
+
+    for (let i=0; i<N; i++) {
+        referralMap[enroll[i]] = i
+        recommendMap[enroll[i]] = referral[i]
+    }
+
+    for (let i=0; i<seller.length; i++) {
+        
+        const sellerName = seller[i]
+        let curMoney = amount[i] * 100
+        let curSeller = sellerName
+
+        while (curSeller !== '-' && curMoney > 0) {
+            
+            const idx = referralMap[curSeller]
+            const referralMoney = Math.floor(curMoney * 0.1)
+            const ownMoney = curMoney - referralMoney
+
+            // 내 돈
+            money[idx] += ownMoney
+
+            curMoney = referralMoney
+            curSeller = recommendMap[curSeller]
+
+            // 1원 미만
+            if (referralMoney < 1) {
+                break
+            }
+        }
+    }
+
+    return money
+}
