385e19ce07ef0833df632465f74421f4c52f2ac9
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-11-28 16:53:34 +0900

    [Gold I] Title: 구간 합 구하기, Time: 944 ms, Memory: 207364 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.js
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.js
@@ -13,46 +13,48 @@ const queries = input.slice(n + 1).map((v) => v.split(' ').map(Number))
 class SegmentTree {
   constructor(arr) {
       this.n = arr.length
-      this.tree = new Array(4 * this.n).fill(0)
-      this.init(arr, 0, this.n-1, 1)
+      this.tree = new Array(2 * this.n).fill(0n)
+      this.build(arr)
   }
 
-  init(arr, start, end, node) {
-    if (start === end) {
-      this.tree[node] = arr[start]
-      return this.tree[node]
-    }
-    const mid = Math.floor((start + end) / 2)
-    this.tree[node] = this.init(arr, start, mid, node * 2) + this.init(arr, mid + 1, end, node * 2 + 1)
-    return this.tree[node]
+  build(arr) {
+      for (let i = 0; i < this.n; i++) {
+          this.tree[this.n + i] = arr[i]
+      }
+
+      for (let i = this.n - 1; i > 0; i--) {
+          this.tree[i] = this.tree[i * 2] + this.tree[i * 2 + 1]
+      }
   }
 
-  // 구간 합 (i부터 j까지)
-  query(start, end, left, right, node) {
+  query(i, j) {
+      i += this.n
+      j += this.n
+      let sum = 0n
 
-    if (right < start || end < left) return 0n
-    if (left <= start && end <= right) return this.tree[node]
-
-    const mid = Math.floor((start + end) / 2)
-    return (
-        this.query(start, mid, left, right, node * 2) +
-        this.query(mid + 1, end, left, right, node * 2 + 1)
-    )
-}
+      while (i <= j) {
+          if (i % 2 === 1) {
+              sum += this.tree[i]
+              i++
+          }
+          if (j % 2 === 0) {
+              sum += this.tree[j]
+              j--
+          }
+          i = Math.floor(i / 2)
+          j = Math.floor(j / 2)
+      }
+      return sum
+  }
   
-  // 값 업데이트
-  update(start, end, idx, diff, node) {
+  update(idx, value) {
+      idx += this.n
+      this.tree[idx] = value
 
-    if (idx < start || idx > end) return
-
-    // 값 갱신
-    this.tree[node] += diff
-
-    if (start !== end) {
-        const mid = Math.floor((start + end) / 2)
-        this.update(start, mid, idx, diff, node * 2)
-        this.update(mid + 1, end, idx, diff, node * 2 + 1)
-    }
+      while (idx > 1) {
+          idx = Math.floor(idx / 2)
+          this.tree[idx] = this.tree[2 * idx] + this.tree[2 * idx + 1]
+      }
   }
 }
 
@@ -60,22 +62,16 @@ const segTree = new SegmentTree(arr)
 
 const answer = []
 
-for (const [a, b, c] of queries) {
+queries.forEach(([a, b, c]) => {
+
+  // a === 1 업데이트
   if (a === 1) {
-      
-    // 업데이트 쿼리
-    const idx = b - 1
-    const newValue = BigInt(c)
-    const diff = newValue - arr[idx]
-    arr[idx] = newValue
-    segTree.update(0, n - 1, idx, diff, 1)
-  
+    segTree.update(b-1, BigInt(c))
+
+  // a === 2 구간 합
   } else if (a === 2) {
-    // 구간 합 쿼리
-    const left = b - 1
-    const right = c - 1
-    answer.push(segTree.query(0, n - 1, left, right, 1))
+    answer.push(segTree.query(b-1, c-1))
   }
-}
+})
 
 console.log(answer.join('\n'))
