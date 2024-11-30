84cb8ffeac731bdd09212872e52ce7aae139b97b
챔경 <139419164+minnnnnk0@users.noreply.github.com>
2024-12-01 02:32:28 +0900

    [level 3] Title: 110 옮기기, Time: 219.86 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
@@ -1,38 +1,34 @@
 function solution(s) {
     const answer = []
 
-    s.forEach((str) => {
+    for (const str of s) {
         
-        const word = []
-        let idx = 0
+        let word = []
         let cnt = 0
 
         // 110 제거
         for (let i=0; i<str.length; i++) {
-            word[idx] = str[i]
-            idx++
+            word.push(str[i])
 
-            if (idx >= 3 && word[idx - 1] === '0' && word[idx - 2] === '1' && word[idx - 3] === '1') {
-                idx -= 3
-                cnt++
-            }
+            if (word.length < 3 || word.slice(-3).join('') !== '110') continue
+            word.splice(-3, 3)
+            cnt++ 
+            
         }
 
-        const string = word.slice(0, idx).join('')
-
-        // 110 삽입
+        const string = word.join('')
         const addNum = '110'.repeat(cnt)
-        const idx0 = string.lastIndexOf('0')
 
-        let result = ''
+        // 마지막 0을 찾고 그 뒤에 110 삽입
+        const idx0 = string.lastIndexOf('0')
+        let result
+        
         if (idx0 < 0) {
-            result = addNum + string // 0 없 -> 맨 앞에 삽입
+            result = addNum + string
         } else {
             result = string.slice(0, idx0 + 1) + addNum + string.slice(idx0 + 1)
         }
-
         answer.push(result)
-    });
-
+    }
     return answer
 }
